fea84dde600951be67b69f0df343ca1368716aa5
Sushma S Bilidale <111302357+Sushma-66@users.noreply.github.com>
2025-11-04 02:02:06 +0000

    Refactor practice_queries.sql for better readability
    Removed comments and added formatting for clarity.

--- practice_queries.sql
+++ practice_queries.sql
@@ -1,6 +1,21 @@
-🧠 LEVEL 1 — Basic Understanding
-Q1.
+--LEVEL 1 — Basic Understanding
 
-Show all columns from the table students.
+  --Q1. Show all columns from the table students.
+
 SELECT *
 FROM students;
+
+--Q2. Show the name and age of all employees from the employees table.
+
+SELECT name, age
+FROM employees;
+
+--Q3. Show the names of employees from employees table whose salary is greater than 50,000.
+
+SELECT name
+FROM employees
+WHERE salary > 50,000;
+
+--LEVEL 2 — Adding Order and Multiple Conditions
+
+

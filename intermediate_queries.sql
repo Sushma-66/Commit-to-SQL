4a74080977caee001e2a8a207375d0251528d97e
Sushma S Bilidale <111302357+Sushma-66@users.noreply.github.com>
2025-11-06 09:10:38 +0000

    Update intermediate_queries.sql

--- intermediate_queries.sql
+++ intermediate_queries.sql
@@ -1,1 +1,24 @@
+--JOINS 
+
+--customer's table                             
+--| customer_id | name  | city     |
+--| ----------- | ----- | -------- |
+--| 1           | Alice | New York |
+--| 2           | Bob   | Chicago  |
+--| 3           | Carol | Miami    |
+
+--orders
+--| order_id | customer_id | product    | amount |
+--| -------- | ----------- | ---------- | ------ |
+--| 101      | 1           | Laptop     | 1200   |
+--| 102      | 2           | Phone      | 800    |
+--| 103      | 1           | Headphones | 150    |
+
+--Products
+
+--| product_id | product_name | price |
+--| ---------- | ------------ | ----- |
+--| 1          | Laptop       | 1200  |
+--| 2          | Phone        | 800   |
+--| 3          | Headphones   | 150   |
 

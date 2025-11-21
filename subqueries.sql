b35db629da7070ab9966d50ec4b1ce84aaba6bbc
Sushma S Bilidale <111302357+Sushma-66@users.noreply.github.com>
2025-11-21 23:44:38 +0000

    Add SQL queries for various data retrieval tasks
    Added SQL queries to list products, show students with high scores, find departments with more employees than IT, and display customers who placed orders in 2024.

--- subqueries.sql
+++ subqueries.sql
@@ -5,3 +5,38 @@ WHERE salary > (
     SELECT AVG(salary) FROM employees
 );
 
+--2. List products that have a price higher than the price of product ID 10.
+SELECT product_name, price
+FROM products
+WHERE price > (
+    SELECT price FROM products WHERE product_id = 10
+);
+
+--3. Show students who scored higher than the minimum score in the class.
+SELECT student_name, score
+FROM students
+WHERE score > (
+    SELECT MIN(score) FROM students
+);
+--4. Find departments that have more employees than the IT department.
+
+SELECT department_name
+FROM departments
+WHERE (
+    SELECT COUNT(*) 
+    FROM employees 
+    WHERE employees.department_id = departments.department_id
+) >
+(
+    SELECT COUNT(*) 
+    FROM employees 
+    WHERE department_id = 3   -- IT dept ID example
+);
+--5. Display customers who placed an order in 2024.
+SELECT customer_name
+FROM customers
+WHERE customer_id IN (
+    SELECT customer_id 
+    FROM orders 
+    WHERE YEAR(order_date) = 2024
+);

407a265a20ea073a911bc8f8c481237ebe8d0990
Sushma S Bilidale <111302357+Sushma-66@users.noreply.github.com>
2026-01-20 15:53:18 +0000

    Implement natural join for salesman, customer, and orders
    Added SQL query to join tables 'salesman', 'customer', and 'orders' using natural joins.

--- intermediate_queries.sql
+++ intermediate_queries.sql
@@ -334,3 +334,54 @@ FROM salesman, customer
 -- Specifies the condition for joining the tables and filtering the data.
 WHERE salesman.city = customer.city;
 
+--2. Join All Tables Uniquely
+
+--Write a SQL statement to join the tables salesman, customer and orders so that the same column of each table appears once and only the relational rows are returned. 
+
+--Sample table: orders
+
+ord_no      purch_amt   ord_date    customer_id  salesman_id
+----------  ----------  ----------  -----------  -----------
+70001       150.5       2012-10-05  3005         5002
+70009       270.65      2012-09-10  3001         5005
+70002       65.26       2012-10-05  3002         5001
+70004       110.5       2012-08-17  3009         5003
+70007       948.5       2012-09-10  3005         5002
+70005       2400.6      2012-07-27  3007         5001
+70008       5760        2012-09-10  3002         5001
+70010       1983.43     2012-10-10  3004         5006
+....
+--View the table
+
+--Sample table: customer
+
+ customer_id |   cust_name    |    city    | grade | salesman_id 
+-------------+----------------+------------+-------+-------------
+        3002 | Nick Rimando   | New York   |   100 |        5001
+        3007 | Brad Davis     | New York   |   200 |        5001
+        3005 | Graham Zusi    | California |   200 |        5002
+        3008 | Julian Green   | London     |   300 |        5002
+        3004 | Fabian Johnson | Paris      |   300 |        5006
+        3009 | Geoff Cameron  | Berlin     |   100 |        5003
+        3003 | Jozy Altidor   | Moscow     |   200 |        5007
+        3001 | Brad Guzan     | London     |       |        5005
+--Sample table : salesman
+
+ salesman_id |    name    |   city   | commission 
+-------------+------------+----------+------------
+        5001 | James Hoog | New York |       0.15
+        5002 | Nail Knite | Paris    |       0.13
+        5005 | Pit Alex   | London   |       0.11
+        5006 | Mc Lyon    | Paris    |       0.14
+        5007 | Paul Adam  | Rome     |       0.13
+        5003 | Lauson Hen | San Jose |       0.12
+
+solution:
+-- Selecting all columns from the result of natural joins between three tables: 'orders', 'customer', and 'salesman'
+SELECT * 
+-- Performing a natural join between 'orders' and 'customer' tables
+FROM orders 
+NATURAL JOIN customer  
+-- Performing another natural join with the result of the previous join and the 'salesman' table
+NATURAL JOIN salesman;
+

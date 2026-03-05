8d655d4aa3e1c416982370ffc7120dc8731b33f9
Sushma S Bilidale <111302357+Sushma-66@users.noreply.github.com>
2026-03-05 23:16:30 +0000

    Add subquery to calculate customer totals

--- subqueries.sql
+++ subqueries.sql
@@ -5,3 +5,17 @@ WHERE salary > (
     SELECT AVG(salary) FROM employees
 );
 
+
+--2. WITH customer_totals AS (
+    SELECT 
+        customer_id,
+        SUM(order_amount) AS total_spent
+    FROM Orders
+    GROUP BY customer_id
+)
+
+SELECT 
+    customer_id,
+    total_spent
+FROM customer_totals
+WHERE total_spent > 80;

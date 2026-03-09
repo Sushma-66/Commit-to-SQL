6a59786f18776a8ceca52dd74696090efe3e0cf6
Sushma S Bilidale <111302357+Sushma-66@users.noreply.github.com>
2026-03-09 15:44:42 +0000

    Add query to retrieve customer spending totals

--- Advanced sql.sql
+++ Advanced sql.sql
@@ -12,3 +12,19 @@ SELECT
 FROM customer_spending
 ORDER BY total_spent DESC
 LIMIT 1;
+
+--2 WITH customer_totals AS (
+    SELECT 
+        customer_id,
+        SUM(order_amount) AS total_spent
+    FROM Orders
+    GROUP BY customer_id
+)
+
+SELECT 
+    c.customer_name,
+    ct.total_spent
+FROM customer_totals ct
+JOIN Customers c
+    ON ct.customer_id = c.customer_id
+ORDER BY ct.total_spent DESC;
